63918a10af937c0cc83a2253866fd7a4d8946919
4uruanna <adrien@villalonga.software>
2025-12-04 15:55:53 +0100

    fix: callable events

--- source/events.php
+++ source/events.php
@@ -5,12 +5,6 @@ namespace Papimod\Cache;
 use Papi\enumerator\AppBuilderEvents;
 
 return [
-    [
-        AppBuilderEvents::BEFORE_DI,
-        DICacheEvent::class
-    ],
-    [
-        AppBuilderEvents::BEFORE_ROUTES,
-        RouteCacheEvent::class
-    ]
+    [AppBuilderEvents::BEFORE_DI, new DICacheEvent],
+    [AppBuilderEvents::BEFORE_ROUTES, new RouteCacheEvent]
 ];
